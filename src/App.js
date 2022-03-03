b8f7d9b02d1bb790545128de4c3326b9d3d32281
Edwin Damanik <edwindamanik5@gmail.com>
2022-03-03 22:39:26 +0700

    Quiz App with Trivia DB by React JS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login.component';
 import Quiz from './components/Quiz/Quiz.component';
-import Timer from './components/Timer/Timer.component';
 import Welcome from './components/Welcome/Welcome.component';
 import firebase from './firebase/firebase.utils';
 function App() {
@@ -12,9 +11,7 @@ function App() {
     firebase.auth().onAuthStateChanged(user => {
       setUser(user)
     })
-  }, [])
-
-  console.log(user);
+  }, []);
 
   const [start, useStart] = useState(false);
 
